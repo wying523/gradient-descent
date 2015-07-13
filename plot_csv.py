690890452ecd4d0f4c88636cb3fd3b2f151d702f
Akshay Chiwhane <achiwhane@gmail.com>
2015-07-12 20:05:42 -0400

    update plot.py

--- plot_csv.py
+++ plot_csv.py
@@ -1,4 +1,4 @@
-#generates a scatterplot with a given .csv file
+
 import sys
 import csv
 import numpy as np
@@ -7,12 +7,23 @@ import matplotlib.pyplot as plt
 
 
 def main():
-  data = list(csv.reader(open(sys.argv[1], "r"), delimiter=","))
+  # data taken from 
+  # https://github.com/vincentarelbundock/Rdatasets/blob/master/csv/datasets/AirPassengers.csv
+  data = list(csv.reader(open("AirPassengers.csv", "r"), delimiter=","))
+
+  #remove labels
+  data.pop(0)
+
+  #remove unnecessary 1st column
+  for i in data:
+    i.pop(0)
 
   x = np.asarray([i[0] for i in data])
   y = np.asarray([i[1] for i in data])
-  z = np.asarray([i[1] for i in data])
-  plt.scatter(x, y, z)
+  plt.xlabel("Year")
+  plt.ylabel('Number of Passengers (in thousands)')
+  plt.title("Air Passengers")
+  plt.scatter(x, y)
   plt.show()
 
 if __name__ == "__main__":
